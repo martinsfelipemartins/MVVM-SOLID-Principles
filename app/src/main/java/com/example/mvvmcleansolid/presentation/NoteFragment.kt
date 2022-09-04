81769d703a8ab36896d4a8e8c84ac49f90825ad4
martinsfelipemartins <martins.felipe1@hotmail.com>
2022-09-04 02:43:11 -0300

    add view model

--- app/src/main/java/com/example/mvvmcleansolid/presentation/NoteFragment.kt
+++ app/src/main/java/com/example/mvvmcleansolid/presentation/NoteFragment.kt
@@ -1,14 +1,24 @@
 package com.example.mvvmcleansolid.presentation
 
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
+import com.example.core.data.Note
 import com.example.mvvmcleansolid.databinding.FragmentNoteBinding
+import com.example.mvvmcleansolid.framework.NoteViewModel
 
 class NoteFragment : Fragment() {
+
+    private lateinit var viewModel: NoteViewModel
+    private var currentNote =
+        Note(title = "", content = "", creationTime = 0L, updateTime = 0L)
 
     lateinit var binding: FragmentNoteBinding
     override fun onCreateView(
@@ -16,17 +26,51 @@ class NoteFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentNoteBinding.inflate(inflater, container, false)
-         return binding.root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        goBack()
+        viewModel = ViewModelProviders.of(this)[NoteViewModel::class.java]
+        addNote()
+        observerSetup()
     }
 
-    private fun goBack() {
+    private fun addNote() {
         binding.floatingActionButtonCheck.setOnClickListener {
-            Navigation.findNavController(it).popBackStack()
+            if (binding.editTextContent.text.toString() != "" || binding.editTextTitle.text.toString() != "") {
+                val time = System.currentTimeMillis()
+                currentNote = Note(
+                    title = binding.editTextTitle.toString(),
+                    content = binding.editTextContent.toString(),
+                    creationTime = time,
+                    updateTime = time
+                )
+                viewModel.saveNote(currentNote)
+            } else {
+                Navigation.findNavController(it).popBackStack()
+            }
         }
     }
+
+    private fun observerSetup() {
+        viewModel.saved.observe(viewLifecycleOwner) {
+            if (it) {
+                hideKeyboard()
+                Toast.makeText(context, "Nota adicionada", Toast.LENGTH_LONG).show()
+                Navigation.findNavController(binding.editTextContent).popBackStack()
+            } else {
+                Toast.makeText(context, "Algo deu errado. Tente Novamente.", Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+    }
+
+    private fun hideKeyboard() {
+        val imm: InputMethodManager =
+            context?.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(binding.editTextTitle.windowToken, 0)
+    }
+
+
 }
